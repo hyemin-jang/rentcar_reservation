f21e86ebeab62dca5a1cb5d9c3ebc722f9508619
hyemin <hmjang28@gmail.com>
2021-08-11 16:23:54 +0900

    DBUtil

--- src/rent/model/util/DBUtil.java
+++ src/rent/model/util/DBUtil.java
@@ -1,5 +1,67 @@
 package rent.model.util;
 
-public class DBUtil {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
+public class DBUtil {
+	private static Properties dbInfo = new Properties();
+	private static Properties sql = new Properties();
+	
+	static {
+		try {
+			dbInfo.load(new FileInputStream("db.properties"));
+			sql.load(new FileInputStream("rentsql.properties"));
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+	
+	public static Properties getSql() {
+		return sql;
+	}
+	
+	
+	public static Connection getConnection() throws SQLException {  
+		return DriverManager.getConnection(dbInfo.getProperty("jdbc.url"), dbInfo.getProperty("jdbc.id"), dbInfo.getProperty("jdbc.pw"));	
+	}
+	
+	public static void close(Connection con, Statement stmt) {
+		try {
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		} catch (SQLException s) {
+			s.printStackTrace();
+		}
+	}
+
+	public static void close(Connection con, Statement stmt, ResultSet rset) {
+		try {
+			if (rset != null) {
+				rset.close();
+				rset = null;
+			}
+			if (stmt != null) {
+				stmt.close();
+				stmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		} catch (SQLException s) {
+			s.printStackTrace();
+		}
+	}
 }
