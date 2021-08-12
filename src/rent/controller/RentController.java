fe3f52b6a5cc203cbd36ae54594ea3a18608c939
jhshin29 <shinji03264@gmail.com>
2021-08-12 11:43:41 +0900

    [Add] Rent a Car

--- src/rent/controller/RentController.java
+++ src/rent/controller/RentController.java
@@ -1,9 +1,25 @@
 package rent.controller;
 
+import java.sql.SQLException;
+
+import probono.exception.NotExistException;
+import probono.model.ProbonoService;
+import rent.view.RunningEndView;
+
 public class RentController {
 
-	public static void addRentList() {
-
+	public static void addRentList(Integer rentId, String endday, Integer user_id, Integer car_id) {
+		try{
+			boolean r = ProbonoService.updateProbono(probonoId, probonoPurpose);
+			RunningEndView.updateView(r, probonoId);			
+		}catch(SQLException s){
+			s.printStackTrace();
+			RunningEndView.showError("프로보노 id로 프로보노 목적 변경 오류 재시도 하세요");
+		}
+//		catch(NotExistException s){
+//			s.printStackTrace();
+//			RunningEndView.showError("프로보노 id 재 확인후 재시도 하세요");
+//		}
 	}
 
 }
--- src/rent/view/RunningStartView.java
+++ src/rent/view/RunningStartView.java
@@ -1,6 +1,7 @@
 package rent.view;
 
 import rent.controller.CarController;
+import rent.controller.RentController;
 import rent.model.dto.CarDTO;
 
 public class RunningStartView {
@@ -25,19 +26,22 @@ public class RunningStartView {
 		// 대여가능한 차량 검색
 		System.out.println("\n***** 대여 가능한 차량 검색 *****");
 		CarController.getCarisRentList();
+		
+		System.out.println("\n***** 차량 대여 *****");
+		RentController.addRentList();
 
 		// 관리자 - 차량 추가
-		System.out.println("\n***** 차량 추가 *****");
-		CarController.addCar(new CarDTO(10, "셀토스", "기아", "SUV", 20000, "0")); // car_id 자동부여 가능????
-		System.out.println("***** 유효하지 않은  값으로 차량 추가 *****");
-		CarController.addCar(new CarDTO(11, "셀토스", "기아", null, 20000, "0"));
-
-		System.out.println("***** 차량 등록 번호로 차량 삭제 *****");
-		CarController.deleteCar();
-		System.out.println("\n***** 없는 번호로 삭제 시도시 *****");
-		CarController.deleteCar();
-
-		System.out.println("\n***** 모든 대여 내역 조회 *****");
-		CarController.getAllRentList();
+//		System.out.println("\n***** 차량 추가 *****");
+//		CarController.addCar(new CarDTO(10, "셀토스", "기아", "SUV", 20000, "0")); // car_id 자동부여 가능????
+//		System.out.println("***** 유효하지 않은  값으로 차량 추가 *****");
+//		CarController.addCar(new CarDTO(11, "셀토스", "기아", null, 20000, "0"));
+//
+//		System.out.println("***** 차량 등록 번호로 차량 삭제 *****");
+//		CarController.deleteCar();
+//		System.out.println("\n***** 없는 번호로 삭제 시도시 *****");
+//		CarController.deleteCar();
+//
+//		System.out.println("\n***** 모든 대여 내역 조회 *****");
+//		CarController.getAllRentList();
 	}
 }
