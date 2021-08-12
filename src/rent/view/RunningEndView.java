a4cbc2a26ddbab70152d442c2c5786cb4085a551
hyemin <hmjang28@gmail.com>
2021-08-12 10:57:20 +0900

    [ADD] addCar, deleteCar, getAllRentList

--- src/rent/view/RunningEndView.java
+++ src/rent/view/RunningEndView.java
@@ -7,16 +7,16 @@ import rent.model.dto.CarDTO;
 public class RunningEndView {
 	
 	// 모든 차량 출력
-	public static void getAllCar(ArrayList<CarDTO> getAllCar){
-		if(getAllCar != null) {
-			int length = getAllCar.size();
+	public static void getAllList(ArrayList list){
+		if(list != null) {
+			int length = list.size();
 			
 			if( length != 0 ){
 				for(int index = 0; index < length; index++){			
-					System.out.println("검색정보" + (index+1) + " - " + getAllCar.get(index));
+					System.out.println("검색정보" + (index+1) + " - " + list.get(index));
 				}
 			}else {
-				System.out.println("요청하신 차량 정보가 존재하지 않습니다.");
+				System.out.println("요청하신 내역이 존재하지 않습니다.");
 			}
 		}else {
 			System.out.println("null은 허용되지 않습니다.");
@@ -27,5 +27,11 @@ public class RunningEndView {
 	// 예외 상황 출력
 	public static void showError(String message){
 		System.out.println(message);		
+	}
+
+	// 정상 수행시 메세지 출력
+	public static void showMessage(String message) {
+		System.out.println(message);
 	}	
+	
 }
